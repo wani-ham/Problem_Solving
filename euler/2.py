d97520eddc5cf2cd8abd0c65ee500cb61d719896
Taewan Ham <neoham7@gmail.com>
2021-01-06 22:49:29 +0900

    Update 2.py

--- euler/2.py
+++ euler/2.py
@@ -2,8 +2,9 @@ a1 = 1
 a2 = 2
 a3 = 3
 tot = 0
+n = 4000000 
 
-while a3 < 4000000:
+while a3 < n:
   a3 = a1 + a2
   if a3 % 2 == 0:
     tot += a3
@@ -11,3 +12,4 @@ while a3 < 4000000:
   a2 = a3 
 
 print(tot + 2) # including the second element
+
