9abff80b103546c1e44566946d6d76d496cef9d4
Taewan Ham <neoham7@gmail.com>
2021-07-16 23:27:35 +0900

    Create 15650.cpp

---
+++ Baekjoon/15650.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void print_arr();
+void bt(int cnt);
+
+int arr[9];
+bool visited[9] = {false, };
+int n, m;
+
+int main() {
+  cin >> n >> m;
+  bt(0);
+  return 0;
+}
+
+void print_arr() {
+  for(int i = 0; i < m; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+  return;
+}
+
+void bt(int cnt) {
+  if(cnt == m) {
+    print_arr();
+    return;
+  }
+
+  for(int i = 1; i <= n; i++) {
+    if(!visited[i]) {
+      visited[i] = true;
+      if(0 == cnt || arr[cnt - 1] < i) {
+        arr[cnt] = i;
+        bt(cnt + 1);
+        visited[i] = false;
+      }
+    }
+    visited[i] = false;
+  }
+  return;
+}
